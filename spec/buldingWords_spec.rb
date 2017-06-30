c9d20df9c52b002c1bfaa67c4733f9bc01f14c13
kleer <kleer@kleer-csd-box>
2017-06-30 09:24:47 -0500

    pantalla de seleccion de nivel

---
+++ spec/buldingWords_spec.rb
@@ -0,0 +1,7 @@
+When(/^selecciono el nivel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^inicia el juego$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
