0985ae7d6b99f9a0c6747df66a470e694cd80417
kleer <kleer@kleer-csd-box>
2017-06-30 12:01:23 -0500

    delete files

--- features/buildingWords.feature
+++
@@ -1,8 +0,0 @@
-Feature:
-	Para poder seleccionar el nivel del juego
-	Quiero configurar el nivel de este
-	Para generar nuevos retos
-
-Scenario: debo poder seleccionar el nivel del juego
-	When selecciono el nivel
-	Then inicia el juego
--- spec/buldingWords_spec.rb
+++
@@ -1,7 +0,0 @@
-When(/^selecciono el nivel$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^inicia el juego$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
